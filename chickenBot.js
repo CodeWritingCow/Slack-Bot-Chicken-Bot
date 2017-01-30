1f9b8ecbd0fb6132db04f3e6b57399f90b563174
Gary Pang <gary.pang@gmail.com>
2017-01-30 02:44:43 -0500

    Update chickenBot.js
    Eliminate spaces in between parameters for controller.hears( ). Chicken
    Bot now responds to user inputs in Slack channels. Previously, Bot
    responded only to direct messages.

--- chickenBot.js
+++ chickenBot.js
@@ -11,11 +11,11 @@ var bot = controller.spawn({
 
 
 // Bot listens for user asking 'why did the chicken cross the road.' Responds by telling joke.
-controller.hears(['why did the chicken cross the road'], 'direct_message, direct_mention, mention', function(bot, message) {
+controller.hears(['why did the chicken cross the road'], 'direct_message,direct_mention,mention', function(bot, message) {
 	bot.reply(message, chickenJokes.getResponse());
 });
 
 // Bot listens for user saying hi or mentioning 'chicken-bot.' Responds with chicken emoji.
-controller.hears(['hi', 'hello', 'howdy'], 'direct_message, direct_mention, mention', function(bot, message) {
+controller.hears(['hi', 'hello', 'howdy'], 'direct_message,direct_mention,mention', function(bot, message) {
 	bot.reply(message, ':chicken:');
 });
