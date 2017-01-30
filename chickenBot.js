092b87e40f231217d792814879b74f86123d7003
Gary Pang <gary.pang@gmail.com>
2017-01-30 03:18:25 -0500

    Add typing indicator
    Bot no longer responds immediately to user. Slack now displays "bot is
    typing ..." message first. Enhance user experience (hopefully!)

--- chickenBot.js
+++ chickenBot.js
@@ -6,16 +6,17 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-	token: process.env.BOT_API_KEY || require('./token')
+	token: process.env.BOT_API_KEY || require('./token'),
+	send_via_rtm: true // This is required to make bot.replyWithTyping() work
 }).startRTM();
 
 
 // Bot listens for user asking 'why did the chicken cross the road.' Responds by telling joke.
 controller.hears(['why did the chicken cross the road'], 'direct_message,direct_mention,mention', function(bot, message) {
-	bot.reply(message, chickenJokes.getResponse());
+	bot.replyWithTyping(message, chickenJokes.getResponse());
 });
 
 // Bot listens for user saying hi or mentioning 'chicken-bot.' Responds with chicken emoji.
 controller.hears(['hi', 'hello', 'howdy'], 'direct_message,direct_mention,mention', function(bot, message) {
-	bot.reply(message, ':chicken:');
+	bot.replyWithTyping(message, ':chicken:');
 });
